24880e33fc2ed19b89cf537e8d5043ee14822af2
Samara Konkol <samara.konkol@catolica.edu.br>
2026-08-22 04:08:50 -0300

    Atualição do backend

--- js/api.js
+++ js/api.js
@@ -1,5 +1,8 @@
-const API_URL =
+const estaEmDesenvolvimento =
     window.location.hostname === "localhost" ||
-    window.location.hostname === "127.0.0.1"
-        ? "http://localhost:3000"
-        : "https://larplus-api.onrender.com";
+    window.location.hostname === "127.0.0.1" ||
+    window.location.protocol === "file:";
+
+const API_URL = estaEmDesenvolvimento
+    ? "http://localhost:3000"
+    : "https://larplus-api.onrender.com";
